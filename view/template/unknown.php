95e3f10c195ddb4e45f217a5e595f828fd88814d
Daniel Kaczorowski <35191108+DanielKaczorowsk@users.noreply.github.com>
2025-01-22 15:15:21 +0100

    Add files via upload

---
+++ view/template/unknown.php
@@ -0,0 +1,9 @@
+<div class="jumbotron text-center">
+  <h1 class="display-4">Witaj !</h1>
+  <p class="lead">Przykro nam ale zabladziles.</p>
+  <hr class="my-4">
+  <p>Wrazie Potrzeby Wcisnij ten przycisk on przekieruje cie na Strone Glowna.</p>
+  <p class="lead">
+    <a class="btn btn-dark btn-lg" href="http://localhost/" role="button">WRACAM !</a>
+  </p>
+</div>
